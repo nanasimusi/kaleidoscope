f71ca2784f4967e382356eb54be89d55ed4b2343
nanasimusi <kazenomatakenji@gmail.com>
2026-02-17 11:42:28 +0900

    Initial Commit

---
+++ kaleidoscope/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  kaleidoscope
+//
+//  Created by KENJI NAKAYAMA on 2026/02/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ kaleidoscope/kaleidoscopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  kaleidoscopeApp.swift
+//  kaleidoscope
+//
+//  Created by KENJI NAKAYAMA on 2026/02/17.
+//
+
+import SwiftUI
+
+@main
+struct kaleidoscopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
